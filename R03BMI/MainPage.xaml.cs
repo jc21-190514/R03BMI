0fcd95f3ed3559e194a4e6f95e5d1856e1fe2742
隼斗 今井 <190514@jc-21.jp>
2021-11-26 11:17:20 +0900

    BMI計算を記述

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -13,12 +13,16 @@ namespace R03BMI
         public MainPage()
         {
             InitializeComponent();
-            creater.Text = "JK3A00 吉澤　毅";
+            creater.Text = "JK3A02 今井隼斗";
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            double temperature = 20.3;  //気温
+            double humidity = 44;   //湿度
+            double discomfort_Index = (0.81 * temperature) + (0.01 * humidity) * ((0.99 * temperature) - 14.3) + 46.3;//不快指数を計算
+            //(0.81*temp)+(0.01*fumi)*((0.99*temp)-14.3)+46.3
+            Console.WriteLine(discomfort_Index);
         }
     }
 }
